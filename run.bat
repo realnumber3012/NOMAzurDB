f2090468d873f8a1915201a5cada2de03033b8ae
DePizzottri <depizzottri@gmail.com>
2016-10-20 18:29:35 +0800

    Multinode example run

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-start sbt "runMain org.mazurdb.TheApp 14725 14780"
-start sbt "runMain org.mazurdb.TheApp 14726 14781"
-start sbt "runMain org.mazurdb.TheApp 0 14782"
+start sbt "-DnodeName=node1" "runMain org.mazurdb.TheApp 14725 14780" > node1.log
+start sbt "-DnodeName=node2" "runMain org.mazurdb.TheApp 14726 14781" > node2.log
+start sbt "-DnodeName=node3" "runMain org.mazurdb.TheApp 0 14782" > node3.log
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-sbt "runMain org.mazurdb.TheApp 14725 14780" > node1.log &
-sbt "runMain org.mazurdb.TheApp 14726 14781" > node2.log & 
-sbt "runMain org.mazurdb.TheApp 0 14782" > node3.log &
+sbt "-DnodeName=node1" "runMain org.mazurdb.TheApp 14725 14780" > node1.log &
+sbt "-DnodeName=node2" "runMain org.mazurdb.TheApp 14726 14781" > node2.log &
+sbt "-DnodeName=node3" "runMain org.mazurdb.TheApp 0 14782" > node3.log &
